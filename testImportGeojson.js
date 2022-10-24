738e444e1febe305cb28fe6a5b3cf09adb598f97
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2022-10-24 17:26:41 -0400

    add foreach examples

--- testImportGeojson.js
+++ testImportGeojson.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import turfArea from '@turf/area';
+import turfLength from '@turf/length';
 
 const readGeojsonFile = function(filePath) {
 
@@ -17,17 +18,43 @@ const calculateArea = function(feature) {
     }
 };
 
+const calculateLength = function(feature) {
+    if (feature.geometry.type === 'LineString' || feature.geometry.type === 'MultiLineString') {
+        const lengthM = turfLength(feature, { units: 'meters' });
+        return lengthM;
+    } else {
+        return undefined;
+    }
+};
+
 const geojson = readGeojsonFile('./test.geojson');
 
+
+const areaAndLengthById = {};
+geojson.features.forEach(function(feature) {
+    const area = calculateArea(feature);
+    const length = calculateLength(feature);
+    areaAndLengthById[feature.properties.id] = {
+        area: area,
+        length: length
+    };
+});
+
+const nums = [1,2,3,4,5,6,7,8,9,10];
+
+nums.forEach(function(num) {
+    console.log(num ** 2);
+});
+/*
 const ids = geojson.features.map(function(feature) {
     return feature.properties.id;
 });
 const names = geojson.features.map(function(feature) {
     return feature.properties.nom;
 });
-
 const areas = geojson.features.map(function(feature) {
     return calculateArea(feature);
 });
+*/
 
-console.log(ids, names, areas);
+console.log(areaById);
